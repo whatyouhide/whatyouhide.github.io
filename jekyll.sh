7b942f7097436efcef5fd16be59f9a5a2bbe445c
Andrea leopardi <an.leopardi@gmail.com>
2022-11-21 19:39:42 +0100

    Fix local jekyll.sh script

--- jekyll.sh
+++ jekyll.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll "$1"
+docker run \
+    -p 4000:4000 \
+    --rm \
+    --volume="$PWD:/srv/jekyll" \
+    --platform linux/amd64 \
+    -it \
+    jekyll/jekyll:4.2.2 \
+    jekyll "$1"
